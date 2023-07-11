5229b13a6fe0b460ce65087a36ad65bba1575c09
Sourav Kumar <souravkumar2001jsr@gmail.com>
2023-07-11 19:20:38 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': 'bfeeb7235bmshf5587645f6ec8c1p1da9afjsnc946bde7202a',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY="1b713a6da4508e989f68d7ffb347b3fa";
